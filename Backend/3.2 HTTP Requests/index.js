5a988cef1492f190f84e485e418b442cfbdb8463
Spencer Kesse <spencerkesse@gmail.com>
2025-08-04 20:47:03 +0100

    Express.js and Node.js

---
+++ Backend/3.2 HTTP Requests/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1> About Me </h1><p> My name is Spencer Kesse </p>');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('<h1> Contact Me </h1><p> +0851292163 </p>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
